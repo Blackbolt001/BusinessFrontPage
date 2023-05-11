74191218c0dbd99ed0332648e48f43375e463b98
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2023-05-11 02:19:03 -0400

    restoreing to backpoint

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Background from "./components/background/Background";
+import Comics from "./components/comics/Comics";
+import "./app.scss";
+
+function App() {
+    return (
+        <div className="app">
+            <Header/>
+           <div className="main">
+            <Background/>
+            <div className="sections">
+            <Main/>
+            <Comics/>
+            </div>
+            </div>
+        </div>
+    );
+}
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+      <div>
+        <Header/>
+      </div>
+    );
+  };
+
+  export default Home;
